e0d8bbb0d47a5eb9eeb3295b8f85a857600035cd
ShekharSumanPatel <shekharsumanpatel@gmail.com>
2022-07-17 00:47:51 +0300

    add glfw submodule

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "adder.h"
+using namespace std;
+
+int main()
+{
+	cout<<"Hey there its me !!!"<<endl;
+	cout<<"The sum of 3.0 and 5.0 = "<<add(3.0, 5.0)<<endl;
+	return 0;
+}
